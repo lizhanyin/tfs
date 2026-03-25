6ccb5abdee17c7382a47f967889db680518f0ecf
GitHub Action <action@github.com>
2026-03-25 05:40:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/lizhanyin/tfs/MyBundle.kt
+++ src/main/kotlin/com/github/lizhanyin/tfs/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lizhanyin.tfs
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/lizhanyin/tfs/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/lizhanyin/tfs/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.lizhanyin.tfs.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.lizhanyin.tfs.MyBundle
+import com.github.lizhanyin.tfs.services.MyProjectService
 import javax.swing.JButton
 
 
